99d9b3bcae7ee635ede4eb1b2a039a472122e9d9
Muralidhar Tellagapalli <tellagapalli.muralidhar@qlik.com>
2026-04-06 15:18:23 +0530

    bump version and update changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,7 @@
+# Changelog
+
+## 0.0.2
+  * Improved 5xx error handling [#8](https://github.com/singer-io/tap-delighted/pull/8)
+
 ## 0.0.1
   * Initial Commit
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages, setup
 
 setup(name="tap-delighted",
-      version="0.0.1",
+      version="0.0.2",
       description="Singer.io tap for extracting data from delighted API",
       author="Stitch",
       url="http://singer.io",
